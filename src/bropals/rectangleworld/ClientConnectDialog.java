8aced477afc7fcc37452a31a08f8a6c0563018e3
RadioLemon <horsentp@gmail.com>
2014-11-25 23:10:39 -0600

    Dialog that request the user for their name and IP address. Need to have it attempt to connect to server now

---
+++ src/bropals/rectangleworld/ClientConnectDialog.java
@@ -0,0 +1,67 @@
+package bropals.rectangleworld;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+	Stores the information about a client that is about to send a request to the server.
+	This is mainly so 
+*/
+public class ClientConnectDialog extends JFrame {
+	
+	/**
+		The text field that will store the name of the client's player.
+	*/
+	private JTextField name;
+	/**
+		The text field that will store the IP of the server's router that
+		the client will try to connect to.
+	*/
+	private JTextField ip;
+	/**
+		The button that will launch the application.
+	*/
+	private JButton go;
+	
+	public ClientConnectDialog() {
+		setTitle("Rectangle World: Join a server");
+		setLayout(new GridLayout(3, 2, 20, 20));
+		name = new JTextField(20);
+		ip = new JTextField(20);
+		go = new JButton("Enter Server");
+		add(new JLabel("Player Name"));
+		add(name);
+		add(new JLabel("IP Address"));
+		add(ip);
+		add(go);
+		setSize(300, 200);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	/**
+		Gets the name that is in the text box.
+	*/
+	public String getName() {
+		return name.getText();
+	}
+	
+	/**
+		Get the address that is in the text box.
+	*/
+	public InetAddress getIPAddress() throws UnknownHostException {
+		return InetAddress.getByName(ip.getText());
+	}
+	
+	public void addGoButtonActionListener(ActionListener al) {
+		go.addActionListener(al);
+	}
+}
--- src/bropals/rectangleworld/RectangleWorldClient.java
+++ src/bropals/rectangleworld/RectangleWorldClient.java
@@ -1,8 +1,44 @@
 package bropals.rectangleworld;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import javax.swing.JOptionPane;
+
 public class RectangleWorldClient {
 
 	public static void main(String[] args) {
-		
+		final ClientConnectDialog dialog = new ClientConnectDialog();
+		dialog.addGoButtonActionListener(new ActionListener() { 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				/*
+					When the go button is pressed, connect the client to the server
+					with the information provided.
+				*/
+				String playerName = dialog.getName();
+				InetAddress address = null;
+				try {
+					address = dialog.getIPAddress();
+ 				} catch(UnknownHostException uhe) {
+					JOptionPane.showMessageDialog(dialog, "Unable to connect to server: " + uhe.toString(), "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				if (address!=null) {
+					dialog.dispose();
+					/*
+						Continue creating the world. Error box popped up if the 
+						address location is unknown (UnknownHostException)
+					*/
+					
+				}
+			}
+		});
+	}
+	
+	private String playerName;
+	
+	public RectangleWorldClient(String playerName, InetAddress address) {
+		this.playerName = playerName;
 	}
 }
