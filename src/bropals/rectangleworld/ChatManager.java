09ac08cf0b52900c09516b5af97ea182f2e328d6
RadioLemon <horsentp@gmail.com>
2014-11-26 09:54:57 -0600

    ChatManager added, just gotta make it draw properly, will do that once we can start testing world behavior

---
+++ src/bropals/rectangleworld/ChatManager.java
@@ -0,0 +1,91 @@
+package bropals.rectangleworld;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+import java.util.Collections;
+import java.awt.Graphics;
+
+/**
+	A chat box that can hold the last few lines of line it got.
+*/
+public class ChatManager {
+	
+	private List<String[]> messages;
+	private char[] currentLine;
+	private boolean chatting;
+	private int maxLines;
+	private int onChar;
+	
+	public ChatManager(int lines, int characterLimit) {
+		chatting = false;
+		maxLines = lines;
+		names = Collections.synchronizedList(new ArrayList<String[]>());
+		currentLine = new char[characterLimit];
+		onChar = 0;
+	}
+	
+	public void drawChatManager(Graphics g) {
+		
+	}
+	
+	public boolean emptyLine() {
+		return onChar==0;
+	}
+	
+	/**
+		Add a chat message to the list, storing the name of the one who said it
+	*/
+	public void getChatMessage(String name, String line) {
+		messages.add(new String[] { name, line });
+		if (messages.size() > maxLines) {
+			messages.remove(messages.size()-1);
+		}
+	}
+	
+	public String[] getMessage(int index) {
+		return messages.get(index);
+	}
+	
+	private void clearCharacterArray() {
+		onChar = 0;
+	}
+	
+	public void startChatting() {
+		chatting = true;
+		onChar = 0;
+	}
+	
+	public void backspace() {
+		onChar--;
+		if (onChar < 0) {
+			cancelChat();
+		}
+	}
+	
+	public void typeKey(char key) {
+		if (isChatting()) {
+			if (onChar < currentLine.length) {
+				currentLine[onChar] = key;
+				onChar++;
+			}
+		}
+	}
+	
+	public boolean isChatting() {
+		return chatting;
+	}
+	
+	public String getTypedLine() {
+		String str = "";
+		for (int i=0; i<onChar; i++) {
+			str += currentLine[i];
+		}
+		return str;
+	}
+	
+	private void cancelChat() {
+		chatting = false;
+		onChar = 0;
+	}
+}
