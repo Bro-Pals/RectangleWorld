ee17d1ad0d4026035ae565ef1f51a17811bfefdc
RadioLemon <horsentp@gmail.com>
2014-11-26 00:10:02 -0600

    GameEventParser woo

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-javac -d class -cp lib/SimpleDrawing.jar src/bropals/rectangleworld/*.java
+javac -d class -cp lib/SimpleDrawing.jar src/bropals/rectangleworld/*.java src/bropals/rectangleworld/event/*.java
 PAUSE
 jar cfm RectangleWorldClient.jar manifest/client/MANIFEST.mf -C class .
 jar cfm RectangleWorldServer.jar manifest/server/MANIFEST.mf -C class .
